ea782cfe144861b1cf2d63319fcd6b22c7f4d5b8
John <mijailhernandez@gmail.com>
2020-09-03 01:11:53 -0500

    Muestra mensaje personalizado por cada input vacio.

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const lastName = document.getElementById('lastname')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
 
-const formIsValid = {
-    firstName: false,
-    lastName: false,
+const validationTable = {
+    firstname: false,
+    lastname: false,
     email: false,
     password: false
 }
@@ -19,37 +19,44 @@ form.addEventListener('submit', (e) => {
 })
 
 firstName.addEventListener('change', (e)=> {
-    if (e.target.value.trim().length > 0) formIsValid.firstName = true
+    if (e.target.value.trim().length > 0) validationTable.firstname = true
     else {
-        formIsValid.firstName = false
+        validationTable.firstname = false
         console.log('First name inválido')
     }
 })
 
 lastName.addEventListener('change', (e) => {
-    if (e.target.value.trim().length > 0) formIsValid.lastName = true
+    if (e.target.value.trim().length > 0) validationTable.lastname = true
     else {
-        formIsValid.lastName = false
+        validationTable.lastname = false
         console.log('Last name inválido')
     }
 })
 
 email.addEventListener('change', (e) => {
     const emailRegex = /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    if (emailRegex.test(e.target.value.trim())) formIsValid.email = true
+    if (emailRegex.test(e.target.value.trim())) validationTable.email = true
     else console.log('email incorrecto')
 })
 
 password.addEventListener('change', (e) => {
     //Should have 1 lowercase letter, 1 uppercase letter, 1 number, and be at least 8 characters long
     const passwordRegex = /(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,}$/
-    if (passwordRegex.test(e.target.value)) formIsValid.password = true
+    if (passwordRegex.test(e.target.value)) validationTable.password = true
     else console.log('password incorrecto')
 })
 
 const validateForm = () => {
-    const formValues = Object.values(formIsValid)
-    const valid = formValues.findIndex(value => value == false)
+    const formValues = Object.values(validationTable)
+    const valid = formValues.findIndex(value => value === false)
     if (valid == -1) form.submit()
-    else console.log('Form invalid')
+    else {
+        const formKeys = Object.keys(validationTable)
+        for (let i = 0; i < formKeys.length; i++) {
+            if (formValues[i] === false) document.querySelector(`.msg-${formKeys[i]}`).classList.add('msg-show')
+            else document.querySelector(`.msg-${formKeys[i]}`).classList.remove('msg-show')
+        }
+        console.log('invalid form')
+    }
 }
